4a00f8cfb906b7b9e6c4b3e78b26858a27d78b7e
Yawen Guan <yawen.guan.email@gmail.com>
2023-09-26 16:46:46 +0200

    Update vscode.

--- modules/applications/vscode.nix
+++ modules/applications/vscode.nix
@@ -1,8 +1,9 @@
 # VSCode settings is synced by the built-in setting-sync.
 
-{ config, pkg, ... }: {
+{ config, pkgs, ... }: {
 	programs.vscode = {
 		enable = true; 
 		enableUpdateCheck = true;
+		package = pkgs.vscode;
 	};
 }
