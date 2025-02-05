a16cb8c0fbef6c7c507495076f9f8536cfcc2f58
Yawen Guan <yawen.guan.email@gmail.com>
2025-02-05 16:32:51 +0100

    Upgrade nixpkgs from 24.05 to 24.11.

--- home-manager/common-linux.nix
+++ home-manager/common-linux.nix
@@ -5,14 +5,7 @@
   ...
 }:
 {
-  imports = [
-    # Temporatory NixGL integration.
-    # TODO: remove it after https://github.com/nix-community/home-manager/pull/5355 is on 24.05.
-    (builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/nix-community/home-manager/8bd6e0a1a805c373686e21678bb07f23293d357b/modules/misc/nixgl.nix";
-      sha256 = "1krclaga358k3swz2n5wbni1b2r7mcxdzr6d7im6b66w3sbpvnb3";
-    })
-  ];
+  imports = [ ];
 
   home.packages = with pkgs; [
     # ===== Utils =====
