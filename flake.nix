fdf9bdcb7a7e5ac3f38ccaa7a027555fc158ca45
Yawen Guan <yawen.guan@proton.me>
2026-02-13 17:11:36 +0100

    Remove home config for guest@pialex-latitude

--- flake.nix
+++ flake.nix
@@ -129,13 +129,6 @@
           };
           modules = [ ./home-manager/homepc.nix ];
         };
-        "guest@pialex-latitude" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs outputs;
-          };
-          modules = [ ./home-manager/latitude.nix ];
-        };
       };
     };
 }
